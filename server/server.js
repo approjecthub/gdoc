b173ab525a32178738ea9b7af1609a80ec65df11
approjecthub <anirbanpal1997@gmail.com>
2023-06-24 12:45:00 +0530

    implemented clustering, sticky sessions

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const gDocument = require('./gDocument')
 const mongoose = require('mongoose')
+const { createAdapter } = require("@socket.io/cluster-adapter");
+const {setupWorker} = require('@socket.io/sticky')
 
 mongoose.connect(process.env.DB_url, {
   useNewUrlParser: true,
@@ -15,18 +17,24 @@ const io = require("socket.io")(3001, {
       methods: ["GET", "POST"],
     },
   })
+io.adapter(createAdapter())
+setupWorker(io)
 
 io.on("connection", socket => {
+  console.info(`socketId: ${socket}, listning connection event...`)
   socket.on("get-document", async documentId => {
+    console.info(`socketId: ${socket}, listning get-document event for docId:${documentId}...`)
     const document = await findOrCreateGDocument(documentId)
     socket.join(documentId)
     socket.emit("load-document", document.data)
 
     socket.on("send-changes", delta => {
+      console.info(`socketId: ${socket}, listning send-changes event...`)
       socket.broadcast.to(documentId).emit("receive-changes", delta)
     })
 
     socket.on("save-document", async data => {
+      console.info(`socketId: ${socket}, listning save-document event...`)
       await gDocument.findByIdAndUpdate(documentId, { data })
     })
   })
