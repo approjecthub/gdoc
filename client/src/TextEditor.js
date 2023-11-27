89166dde752ab45775d8ff2d7e988fb94130c3c8
approjecthub <anirbanpal1997@gmail.com>
2023-11-27 10:07:26 +0530

    image upload feature added

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
-import {io} from 'socket.io-client'
-import {useParams} from 'react-router-dom'
+import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -12,115 +12,152 @@ const TOOLBAR_OPTIONS = [
   [{ color: [] }, { background: [] }],
   [{ script: "sub" }, { script: "super" }],
   [{ align: [] }],
-  ["image", "blockquote", "code-block"],
+  ["image", "blockquote", "code-block", "link"],
   ["clean"],
-]
+];
 
 export default function TextEditor() {
-  const {id: documentId} = useParams()
+  const { id: documentId } = useParams();
   const container = useRef(null);
-  const [isQuillEnabled, setIsQuillEnabled] = useState(false)
-  const [socket, setSocket] = useState(null)
-  const [quill, setQuill] = useState(null)
-
-  useEffect(()=>{
-    const s = io("http://localhost:3001/")
-    setSocket(s)
-
-    return ()=>{
-      s.disconnect()
-    }
-  }, [])
-
-  useEffect(()=>{
-    if(!socket || !quill) return
-
-    const saveHandler = (e)=>{
-      if((e.ctrlKey || e.metaKey) && e.keyCode === 'S'.charCodeAt(0)){
-        e.preventDefault()
-        socket.emit('save-document', quill.getContents())
-      }
-    }
-    document.addEventListener('keydown', saveHandler)
-
-    socket.once("load-document", doc=>{
-      quill.setContents(doc)
-      quill.enable()
-      setIsQuillEnabled(true)
-    })
-    socket.emit('get-document', documentId)
-
-    return ()=>{
-      document.removeEventListener('keydown', saveHandler)
-    }
-  }, [socket, quill, documentId])
-
-  useEffect(()=>{
-    if(!socket || !quill) return
-    if(isQuillEnabled){
-      const interval = setInterval(()=>{
-          socket.emit('save-document', quill.getContents())
-        
-      }, 10*1000)
-  
-      return ()=>{
-        clearInterval(interval)
-      }
-    }
-
-  }, [socket, quill, isQuillEnabled])
-
-  useEffect(()=>{
-    if(!socket || !quill) return 
-
-    const handler = (delta, oldData, source)=>{
-      if(source!== 'user') return 
-       socket.emit("send-changes", delta)
-    }
-    quill.on('text-change', handler)
-
-    return ()=>{
-      quill.off('text-change', handler)
-    }
-  }, [socket, quill])
-
-  useEffect(()=>{
-    if(!socket || !quill) return 
-
-    const handler = (delta)=>{
-      quill.updateContents(delta)
-    }
-    socket.on('receive-changes', handler)
-
-    return ()=>{
-      socket.off('receive-changes', handler)
-    }
-  }, [socket, quill])  
+  const [isQuillEnabled, setIsQuillEnabled] = useState(false);
+  const [socket, setSocket] = useState(null);
+  const [quill, setQuill] = useState(null);
 
   useEffect(() => {
+    const s = io("http://localhost:3001/");
+    setSocket(s);
+
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!socket || !quill) return;
+
+    const saveHandler = (e) => {
+      if ((e.ctrlKey || e.metaKey) && e.keyCode === "S".charCodeAt(0)) {
+        e.preventDefault();
+        socket.emit("save-document", quill.getContents());
+      }
+    };
+    document.addEventListener("keydown", saveHandler);
+
+    socket.once("load-document", (doc) => {
+      quill.setContents(doc);
+      quill.enable();
+      setIsQuillEnabled(true);
+    });
+    socket.emit("get-document", documentId);
+
+    return () => {
+      document.removeEventListener("keydown", saveHandler);
+    };
+  }, [socket, quill, documentId]);
+
+  useEffect(() => {
+    if (!socket || !quill) return;
+    if (isQuillEnabled) {
+      const interval = setInterval(() => {
+        socket.emit("save-document", quill.getContents());
+      }, 10 * 1000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [socket, quill, isQuillEnabled]);
+
+  useEffect(() => {
+    if (!socket || !quill) return;
+
+    const handler = (delta, oldData, source) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+    quill.on("text-change", handler);
+
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (!socket || !quill) return;
+
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+    socket.on("receive-changes", handler);
+
+    return () => {
+      socket.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (!socket) return;
     const div = document.createElement("div");
-    const parentNode = container.current
+    const parentNode = container.current;
     if (parentNode) {
       parentNode.append(div);
     }
 
     const q = new Quill(div, {
       theme: "snow",
-      modules:{
-        toolbar: TOOLBAR_OPTIONS
-      }
+      modules: {
+        toolbar: TOOLBAR_OPTIONS,
+      },
     });
-    q.disable()
-    q.setText('Loading...')
-    setIsQuillEnabled(false)
-    setQuill(q)
+
+    const imageHandler = () => {
+      const input = document.createElement("input");
+
+      input.setAttribute("type", "file");
+      input.setAttribute("accept", "image/*");
+      input.click();
+
+      input.onchange = async () => {
+        var file = input.files[0];
+        var formData = new FormData();
+
+        formData.append("image", file, file.name);
+
+        const requestOptions = {
+          method: "POST",
+          body: formData,
+          redirect: "follow",
+        };
+        try {
+          const uploadResponse = await fetch(
+            "http://localhost:3001/media",
+            requestOptions
+          ).then((response) => response.json());
+          const delta = q.insertEmbed(
+            q.getSelection().index,
+            "image",
+            uploadResponse?.imagePath
+          );
+          socket.emit("send-changes", delta);
+        } catch (err) {
+          console.log("***** Error while uploading image : ", err);
+        }
+      };
+    };
+    const toolbar = q.getModule("toolbar");
+    toolbar.addHandler("image", imageHandler);
+
+    q.disable();
+    q.setText("Loading...");
+    setIsQuillEnabled(false);
+    setQuill(q);
 
     return () => {
-        if (parentNode?.innerHTML) {
-          parentNode.innerHTML = "";
-        }
+      if (parentNode?.innerHTML) {
+        parentNode.innerHTML = "";
+      }
     };
-  }, []);
+  }, [socket]);
 
   return <div className="container" ref={container}></div>;
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,59 @@
 require('dotenv').config()
 const gDocument = require('./gDocument')
 const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+const app = express();
+const multer = require('multer')
+const path = require('path')
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST']
+}))
+app.use(express.static(path.join(__dirname, 'images')))
+
+const MIME_type_mp = {
+  'image/png':'png',
+  'image/jpeg':'jpg',
+  'image/jpg':'jpg'
+}
+
+const storage = multer.diskStorage({
+  destination: (req,file, cb)=>{
+      let isValid = MIME_type_mp[file.mimetype]
+      let error = new Error('file mime type not valid')
+      if (isValid){
+          error = null
+      }
+      cb(error, path.join(__dirname,'images'))
+  },
+  filename:(req,file,cb)=>{
+      const name= file.originalname.split(' ').join('-')
+      const ext = MIME_type_mp[file.mimetype]
+      cb(null, name+'-'+Date.now()+'.'+ext)
+  }
+})
+
+app.post('/media', multer({storage:storage}).single('image'), async(req,res)=>{
+  const url = req.protocol+'://'+req.get('host')
+  try{     
+      res.status(201).send({imagePath:url+'/' +req.file.filename})
+  }
+  catch(err){
+      res.status(400).send(err.message)
+  }
+})
+
+const http = require('http')
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+});
 
 mongoose.connect(process.env.DB_url, {
   useNewUrlParser: true,
@@ -9,13 +62,6 @@ mongoose.connect(process.env.DB_url, {
   useCreateIndex: true
 })
 
-const io = require("socket.io")(3001, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
-    },
-  })
-
 io.on("connection", socket => {
   socket.on("get-document", async documentId => {
     const document = await findOrCreateGDocument(documentId)
@@ -23,6 +69,7 @@ io.on("connection", socket => {
     socket.emit("load-document", document.data)
 
     socket.on("send-changes", delta => {
+      console.log('###### delta: ', delta);
       socket.broadcast.to(documentId).emit("receive-changes", delta)
     })
 
@@ -41,3 +88,8 @@ async function findOrCreateGDocument(id){
 
   return await gDocument.create({_id:id, data: ''})
 }
+
+const PORT = process.env.PORT || 3001
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
